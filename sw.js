47294905f79d9baa8b0bddee2ee1d0748d9b5635
xindi1 <robert.graham411@gmail.com>
2026-06-09 22:17:52 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('seq-plate-v6').then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.json'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
