b6e5cd41041766986e359fa83e5e1cdd21d248d5
xindi1 <robert.graham411@gmail.com>
2026-06-09 22:28:21 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('seq-plate-v6').then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.json'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
+self.addEventListener('install',e=>{e.waitUntil(caches.open('seq-plate-v7').then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.json'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
