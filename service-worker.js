eeecd3c87d1f3e76c57cb275ce2c2400929e7905
xindi1 <robert.graham411@gmail.com>
2026-06-09 21:29:14 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE='sequence-plate-v4';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
